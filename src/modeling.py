38cf57f7ef7041e9387f8c72ef4fb5297c662080
DEBBIH <tarek.debbih@etu.unistra.fr>
2026-04-22 10:16:09 +0200

    add missing import in modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
